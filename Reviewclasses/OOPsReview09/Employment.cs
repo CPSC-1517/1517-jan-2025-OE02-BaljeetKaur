43877755a2158c9c5064d680696120514d28da3a
BaljeetK79 <baljeetk@nait.ca>
2025-01-09 21:10:04 -0700

    Class Libray for Employemnt with Properties defined

---
+++ Reviewclasses/OOPsReview09/Employment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview09
+{
+    public class Employment
+    {
+        private string _Title;
+        public string Title
+        {
+            get { return _Title; }
+            set
+            {
+                if (IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("The Title is required parameter");
+
+                    _Title = value;
+                }
+            }
+        }
+
+        private bool IsNullOrWhiteSpace(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
