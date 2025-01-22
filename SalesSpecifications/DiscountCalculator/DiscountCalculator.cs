18c436c61f7789d8035beeab017b5c885240efc2
BaljeetK79 <baljeetk@nait.ca>
2025-01-21 20:56:22 -0700

    Test2: Implementation of TDD Example: Discount Claculator for 4 specifications

---
+++ SalesSpecifications/DiscountCalculator/DiscountCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscountCalculator
+{
+    public class DiscountCalculator
+    {
+        public decimal? CalculateDiscountPercentage( decimal saleAmount)
+        {
+            if (saleAmount < 100)
+                return 0;
+              else if (saleAmount >= 100 && saleAmount < 500)
+                  return 10;
+              else if (saleAmount >= 500 && saleAmount < 1000)
+                 return 20;
+            return 30;
+
+            
+
+                    
+        }
+    }
+}
