d514bcfd56a80f703a3b1422589ad6088a7b9ec3
BaljeetK79 <baljeetk@nait.ca>
2025-02-25 20:35:26 -0700

    Creation of Blazor input components for data entry

---
+++ BlazorWebSolutionControls/BlazorWebApp/Models/PersonIntro.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp.Models
+
+{
+    public class PersonIntro
+    {
+        public int ValueId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+    }
+}
+
+
